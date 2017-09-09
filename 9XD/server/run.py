2f3831d9f90c542955f5dfd3f8b36f45723a6513
jungwinter <wintermy201@gmail.com>
2017-09-09 13:48:41 +0900

    Route to get, post url
    Signed-off-by: jungwinter <wintermy201@gmail.com>

--- 9XD/server/run.py
+++ 9XD/server/run.py
@@ -1,10 +1,29 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "It's work!"
+
+@app.route("/post", methods=["POST"])
+def post():
+    data = request.json
+    
+    if data is None:
+        return jsonify(status="OK", message="data is null"), 200
+
+    message = data
+    return jsonify(status="OK", message=message), 200
+
+@app.route("/get/")
+@app.route("/get/<int:item_id>", methods=["GET"])
+def get(item_id=None):
+    if item_id is None:  # case: /get, /get/
+        return jsonify(message="default get json")
+    # case: /get/<item_id>
+    return jsonify(id=item_id, message="42")
+
 
 
 if __name__ == '__main__':
